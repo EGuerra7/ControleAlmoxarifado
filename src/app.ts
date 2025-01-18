2f50f348f2cb46d27989610d7d92a2e2708971e5
EGuerra7 <erickmuzzi@gmail.com>
2025-01-18 20:30:59 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { adminRoutes } from './http/controllers/userAdmin/routes'
 import { productRoutes } from './http/controllers/products/routes'
 import { loanRoutes } from './http/controllers/loans/routes'
 
 export const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+})
 
 app.register(adminRoutes)
 app.register(productRoutes)
